83628ba6be9c1cb70951927f102c96ee6aad43a3
Vilmer Fall <Vilmer.fall@gmail.com>
2026-03-25 16:23:52 +0100

    init commit

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red: "#BE1E2D",
+          blue: "#1A2E44",
+          "off-black": "#1A1C1F",
+          "off-white": "#F9F9FD",
+        },
+      },
+      fontFamily: {
+        serif: ["var(--font-newsreader)", "serif"],
+        sans: ["var(--font-work-sans)", "sans-serif"],
+      },
+    },
+  },
+} satisfies Config;
